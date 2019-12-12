976f3d49361d62fa4dba575b733656aff421a6a9
Martin <martin.gobert@adeo.com>
2019-12-12 19:25:34 +0100

    add  eleves parti

--- index.php
+++ index.php
@@ -27,7 +27,40 @@
 
 
     <!-- Contenu à afficher si l'utilisateur est un élève -->
-        <!-- Liste des moyennes par matière pour l'élève connecté -->
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">matiere</th>
+            <th scope="col">note</th>
+            <th scope="col">Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+            $req1 = $bdd->query("SELECT  matiere  from utilisateurs WHERE type='prof'" );
+           while($matiere = $req1->fetch()) {
+               $req = $bdd->prepare("SELECT AVG( notes.note) from notes, utilisateurs , examens WHERE  utilisateurs.matiere = ? and examens.prof_id = utilisateurs.id_utilisateur and notes.ELEVE_ID = 3 and notes.EXAMEN_ID = examens.id    ");
+               $req->execute([$matiere['matiere']]);
+
+               ?>
+                   <tr>
+
+                       <th scope="row"><?php echo $matiere['matiere'] ?></th>
+                       <td ><?php echo $req->fetchAll()[0][0] ?></td>
+                       <td>
+                           <a href="recap_matiere.php?matiere=<?php echo $matiere['matiere']; ?>">
+                               <button type="button" class="btn btn-info">Consulter</button>
+                           </a>
+                       </td>
+                   </tr>
+               <?php
+           }
+
+
+
+
+        ?>
     <!-- Fin du contenu -->
 </div>
 
--- recap_matiere.php
+++ recap_matiere.php
@@ -3,9 +3,35 @@
 ?>
 
 <div class="container">
-    <h1>Notes obtenues en <!-- Matière à insérer --></h1>
+    <h1>Notes obtenues en <?php echo $_GET['matiere'] ?></h1>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">id</th>
+            <th scope="col">date examens</th>
+            <th scope="col">type</th>
+            <th scope="col">coef</th>
+            <th scope="col">note</th>
+        </tr>
+        </thead>
+        <tbody>
+    <?php
+    $req = $bdd->query("SELECT examens.date_examen , examens.type, examens.coef, notes.note from notes, utilisateurs , examens WHERE  utilisateurs.matiere = 'math' and examens.prof_id = utilisateurs.id_utilisateur and notes.ELEVE_ID = 3 and notes.EXAMEN_ID = examens.id    ");
+    while ($resultat = $req->fetch()) {
+        ?>
+    <tr>
+        <th scope="row"><?php echo $resultat['date_examen'] ?></th>
+        <td ><?php echo $resultat['date_examen'] ?></td>
+        <td> <?php echo $resultat['type'] ?></td>
+        <td> <?php echo $resultat['coef'] ?></td>
+        <td><?php echo $resultat['note'] ?></td>
+    </tr>
+        <?php
 
-    
+    }
+
+
+    ?>
 </div>
 
 
